758ba7a61d7b956ff2573e89866879521a2a1df2
alireza-astane <apoorhass@gmail.com>
2025-02-18 16:48:56 +0330

    add print(4)

--- example.py
+++ example.py
@@ -1,18 +1,18 @@
-# imports 
-import numpy as np 
+# imports
+import numpy as np
 import matplotlib.pyplot as plt
 
-#define a function
+
+# define a function
 def sin(x):
     return np.sin(x)
 
 
-
-
 # create plot
-x = np.linspace(0,10,1000)
+x = np.linspace(0, 10, 1000)
 y = sin(x)
 
-#save plot and the numpy file
+# save plot and the numpy file
 
 
+print(2 + 2)
